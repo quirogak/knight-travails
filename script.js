00200107be31f14690694229a60449889ee224fa
quirogak <quirogakill@gmail.com>
2023-02-17 14:42:55 -0500

    Made a function that returns every possible move.
    The function filters every movement out of the board that the knight could make in a given position.

--- script.js
+++ script.js
@@ -18,13 +18,49 @@ const createKnight = () => {
   const possibleKnightMoves = (board, currentPos) => {
     const possibleMoves = [];
 
-    // conditionals to remove possible out of board moves.
+    possibleMoves.push(
+      [currentPos[0] + 2, currentPos[1] + 1],
+      [currentPos[0] + 1, currentPos[1] + 2],
+      [currentPos[0] - 1, currentPos[1] + 2],
+      [currentPos[0] - 2, currentPos[1] + 1],
+      [currentPos[0] - 2, currentPos[1] - 1],
+      [currentPos[0] - 1, currentPos[1] - 2],
+      [currentPos[0] + 1, currentPos[1] - 2],
+      [currentPos[0] + 2, currentPos[1] - 1]
+    ); // push every possible move of a knight in the current position.
 
-    possibleMoves.push([currentPos[0] + 3, currentPos[0] + 1]); // push every possible move of a knight in that situation.
+    // clean out of board moves
 
-    // return every possible movement from every position on the board.
+    const stringPossibleMoves = [];
 
-    return possibleMoves;
+    const stringBoardPositions = [];
+
+    for (let i = 0; i < possibleMoves.length; i++) {
+      stringPossibleMoves.push(
+        Object.values(possibleMoves[i]).toString().replace(",", "")
+      ); // get every possible move as a two value string, then push it into an array.
+    }
+
+    for (let i = 0; i < board.length; i++) {
+      stringBoardPositions.push(
+        Object.values(board[i]).toString().replace(",", "")
+      ); // get every board position as a two value string, then push it into an array .
+    }
+
+    const onlyPossibleMoves = [];
+
+    for (let i = 0; i < stringPossibleMoves.length; i++) {
+      if (stringBoardPositions.includes(stringPossibleMoves[i])) {
+        onlyPossibleMoves.push(possibleMoves[i]); // push every possible (correct) position.
+      }
+    }
+
+    if (!stringBoardPositions.includes(currentPos.toString().replace(",", "")))
+      return "Please enter a valid position"; // if the user enters a non-existent position
+
+      
+    return onlyPossibleMoves;
+
   };
 
   return possibleKnightMoves(createMatrix(8), [0, 0]);
