69fdb2ad4fcd032bcab0b6121fa141b2b6d26402
quirogak <quirogakill@gmail.com>
2023-03-02 12:43:48 -0500

    Finished search algorithm. Now the goal is to convert the info that the search algorithm gives, into an easily readable path.

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ const createKnight = (() => {
     return onlyPossibleMoves;
   };
 
-
+  let isAMove = false
 
 
   const knightMoves = (initialPos, finalPos) => {
@@ -80,6 +80,7 @@ const createKnight = (() => {
   
     const initialNode = Node(initialPos,possibleKnightMoves(createMatrix(8),initialPos))
     const finalNode = Node(finalPos,possibleKnightMoves(createMatrix(8),finalPos))
+
 
     const toStringMoves = (Node) => {
 
@@ -103,45 +104,60 @@ const createKnight = (() => {
       
       })
 
-      
-
       return moves
 
     }
+
+    // the difference between this algorithm and the previous one (see older commits), is that we are visiting the possible nodes using a sort-of breadth-first search, instead of a depth first.
 
     if(!visitedNodes.includes(initialNode.data.toString())) console.log(initialNode)
     
 
     const searchCommonMoves = (arr1, arr2) => arr1.some(item => arr2.includes(item))
 
+    const searchCommonMoves2 = (arr1, arr2) => {arr1.some(item => arr2.includes(item))
+      console.log(arr1)
+      console.log(arr2)}
+
    
-    if (searchCommonMoves(toStringMoves(initialNode),toStringMoves(finalNode))) console.log("connected") // base case, the final node and initial node are connected by at least one move.
+    if (searchCommonMoves(toStringMoves(initialNode),toStringMoves(finalNode))) return null // base case, the final node and initial node are connected by at least one move.
     
-    if(visitedNodes.includes(initialNode.data.toString())) return null
+    
+    if(visitedNodes.includes(initialNode.data.toString())) return null 
 
-   
+    visitedNodes.push(initialNode.data.toString()) 
 
-    visitedNodes.push(initialNode.data.toString())
-
+    
     initialNode.moves.forEach(element => {
 
-      if (searchCommonMoves(toStringMoves(possibleKnightMoves(createMatrix(8),element)),toStringMoves(finalNode))) console.log("success") // here we are going a layer deep of possible moves.
-      // if there isn't any movement in common between the finalNode moves, and the current layer, go a layer deeper of possible moves.
+      console.log(element)
+      console.log(possibleKnightMoves(createMatrix(8),element))
+
+      searchCommonMoves2(toStringMoves(possibleKnightMoves(createMatrix(8),element)),toStringMoves(finalNode))
+
+      if (searchCommonMoves(toStringMoves(possibleKnightMoves(createMatrix(8),element)),toStringMoves(finalNode))){
+        console.log("hit")
+        isAMove = true
+      }  // here we are going a layer deep of possible moves.
   
     });
 
-    
+    console.log(isAMove)
+    if(isAMove === true) return null; // if there is a movement in common in at least one move Node, we won't run knightMoves again, but we will evaluate the rest of the moves.
 
+    // if there isn't any movement in common between the finalNode moves, and the current layer, go a layer deeper of possible moves.
+    initialNode.moves.forEach(element => {
+      knightMoves(element,finalPos)
+    })
+    
   }
 
-  console.log(possibleKnightMoves(createMatrix(8),[1,0]))
+  console.log(possibleKnightMoves(createMatrix(8),[7,7]))
   
 
-
-  
  
   return { knightMoves };
 })();
 
 
-console.log(createKnight.knightMoves([4, 0], [1,0]));
+console.log(createKnight.knightMoves([0, 0], [7,7]));
